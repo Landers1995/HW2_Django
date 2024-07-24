a6b9f90d3ddd61bfee30084e2a76c96272982f57
Ilya Travkin <il-travkin@yandex.ru>
2024-07-24 11:32:27 +0300

    migrations

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_URL = 'media/'
+MEDIA_URL = 'product/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'product')
