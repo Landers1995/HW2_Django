aaa691f968ac9d0795a3fa7aa2c5d01fa898a7dd
Ilya Travkin <il-travkin@yandex.ru>
2024-08-07 11:58:21 +0300

    develop 07.08 2

--- articles/urls.py
+++ articles/urls.py
@@ -2,14 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from articles.apps import ArticlesConfig
-from articles.views import ArticleCreateView, ArticleListView
+from articles.views import ArticleCreateView, ArticleUpdateView, ArticleListView, ArticleDetailView
 
 app_name = ArticlesConfig.name
 
 urlpatterns = [
     path("create/", ArticleCreateView.as_view(), name="create"),
     path("", ArticleListView.as_view(), name="list"),
-    # path("view/<int:pk>/", ..., name="view"),
-    # path("edit/<int:pk>/", ..., name="edit"),
+    path("view/<int:pk>/", ArticleDetailView.as_view(), name="view"),
+    path("edit/<int:pk>/", ArticleUpdateView.as_view(), name="edit"),
     # path("delete/<int:pk>/", ..., name="delete"),
     ]
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, UpdateView, ListView, DetailView
 from articles.models import Article
 
 
@@ -10,5 +10,15 @@ class ArticleCreateView(CreateView):
     success_url = reverse_lazy('articles:list')
 
 
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'description',)
+    success_url = reverse_lazy('articles:list')
+
+
 class ArticleListView(ListView):
+    model = Article
+
+
+class ArticleDetailView(DetailView):
     model = Article
